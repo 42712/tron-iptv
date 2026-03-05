d8d9dc845cb75586be2b878ac7196876fc1a706d
42712 <115371094+42712@users.noreply.github.com>
2026-03-05 17:42:30 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+```javascript
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+
+caches.open("troniptv").then(cache=>{
+
+return cache.addAll([
+"/",
+"/index.html",
+"/style.css",
+"/app.js"
+])
+
+})
+
+)
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(res=>{
+
+return res || fetch(e.request)
+
+})
+
+)
+
+})
+```
