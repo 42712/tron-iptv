51edbc0b6b281215f2470a2abeb0882b947a3b64
42712 <115371094+42712@users.noreply.github.com>
2026-03-05 10:32:42 -0300

    Update player.js

--- player.js
+++ player.js
@@ -1,29 +1,55 @@
+```javascript
 const video = document.getElementById("video")
 
 function play(url){
 
 if(Hls.isSupported()){
 
-const hls = new Hls()
+const hls=new Hls()
+
 hls.loadSource(url)
+
 hls.attachMedia(video)
 
 }else{
 
-video.src = url
+video.src=url
 
 }
 
 }
 
-function addItem(name,url){
+function togglePassword(){
 
-const item = document.createElement("div")
-item.className="item"
-item.innerText=name
+let p=document.getElementById("pass")
 
-item.onclick=()=>play(url)
-
-document.getElementById("list").appendChild(item)
+if(p.type==="password"){
+p.type="text"
+}else{
+p.type="password"
+}
 
 }
+
+function searchItem(){
+
+let input=document.getElementById("search").value.toLowerCase()
+
+let cards=document.querySelectorAll(".card")
+
+cards.forEach(card=>{
+
+if(card.innerText.toLowerCase().includes(input)){
+
+card.style.display="block"
+
+}else{
+
+card.style.display="none"
+
+}
+
+})
+
+}
+```
